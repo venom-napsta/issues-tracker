8bbd86b3b1e7c9119fe7c6fbc07efcbadd631d64
Anesu Napsta <napstakid@gmail.com>
2024-02-18 12:20:58 +0200

    Add Delete dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,46 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import { TrashIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface IIssueId {
   issueId: number;
 }
 const DeleteIssueButton = ({ issueId }: IIssueId) => {
-  return <Button color="red">Delete Issue {issueId}</Button>;
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Revoke access</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content style={{ maxWidth: 450 }}>
+          <AlertDialog.Title>Revoke access</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure? This action cannot be undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red">
+                Revoke access
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <Button color="red">
+        <TrashIcon />
+        <Link href={`/issues`}>Delete Issue {issueId}</Link>
+      </Button>
+    </>
+  );
 };
 
 export default DeleteIssueButton;
