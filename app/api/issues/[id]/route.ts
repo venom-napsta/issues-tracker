553e724a8505fe4ba3f8d88eda1e912a05471c0a
Anesu Napsta <napstakid@gmail.com>
2024-03-05 01:14:48 +0200

    Enhance API to assign Issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from "@/app/issueValidationSchema";
+import { issueSchema, patchIssueSchema } from "@/app/issueValidationSchema";
 import prisma from "@/prisma/client";
 // import delay from "delay";
 import { getServerSession } from "next-auth";
@@ -11,18 +11,27 @@ interface IParams {
 
 // Edit Issue
 export async function PATCH(request: NextRequest, { params }: IParams) {
-  // if (!params.id)
-  //   NextResponse.json({ error: "Invalid ID provided" }, { status: 404 });
+  if (!params.id)
+    NextResponse.json({ error: "Invalid ID provided" }, { status: 404 });
 
-  const session = await getServerSession(authOptions);
-  if (!session)
-    return NextResponse.json({ error: "Not authorised" }, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session)
+  //   return NextResponse.json({ error: "Not authorised" }, { status: 401 });
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid User" }, { status: 400 });
+  }
 
   // Find issue
   const issue = await prisma.issue.findUnique({
@@ -32,10 +41,11 @@ export async function PATCH(request: NextRequest, { params }: IParams) {
     return NextResponse.json({ error: "Issue not found" }, { status: 404 });
 
   const updatedIssue = await prisma.issue.update({
-    where: { id: issue?.id },
+    where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description,
+      title: title,
+      description: description,
+      assignedToUserId: assignedToUserId,
     },
   });
 
@@ -44,9 +54,9 @@ export async function PATCH(request: NextRequest, { params }: IParams) {
 
 // Delete Issue
 export async function DELETE(request: NextRequest, { params }: IParams) {
-  const session = await getServerSession(authOptions);
-  if (!session)
-    return NextResponse.json({ error: "Not authorised" }, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session)
+  //   return NextResponse.json({ error: "Not authorised" }, { status: 401 });
 
   request = request;
   const issue = await prisma.issue.findUnique({
--- app/issueValidationSchema.ts
+++ app/issueValidationSchema.ts
@@ -5,6 +5,26 @@ export const issueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
   description: z
     .string()
+    .min(1, "...")
+    .max(255, "Max number of charaters is 300"),
+});
+
+// assignedToUserID:statua will need values for title and desc
+// it wouldn't be flexible for updating some properties
+// Hence it should accept diff kinds of data. If edit, properties should be optional
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255).optional(),
+  description: z
+    .string()
     .min(1, "Description is required.")
-    .max(300, "Max number of charaters is 300"),
+    .max(500, "Max number of charaters is 500")
+    .optional(),
+
+  // If we include it we have to provide a value
+  assignedToUserId: z
+    .string()
+    .min(1, "Assigned To User ID is Required.")
+    .max(255, "Max number of charaters is 255")
+    .optional()
+    .nullable(),
 });
