ec97074f9cb234d8c933e06ab8b73d8799f91806
Anesu Napsta <napstakid@gmail.com>
2024-03-23 10:31:24 +0200

    Implement Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div className="mt-30">
-      <Pagination currentPage={3} itemCount={25} pageSize={10} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams?.page)}
+      />
     </div>
   );
 }
